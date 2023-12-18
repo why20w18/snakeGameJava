c4423524389800e5f595d04cd3244168294d7282
why20w18 <enco3162@gmail.com>
2023-12-18 14:18:27 +0300

    ekran ortalaniyor ve yilan komponenti eklendi

---
+++ src/ekran.java
@@ -0,0 +1,33 @@
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+
+
+
+public class ekran extends JFrame{
+    
+    private final int eGenislik = 600;
+    private final int eYukseklik = 600;
+    
+    public ekran(){
+        setTitle("Yýlan Oyunu | Sürüm 1");
+        ekranOrtala(eGenislik, eYukseklik);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    
+        yilanVeCerceve cerceve = new yilanVeCerceve();
+        add(cerceve);
+        
+        setVisible(true);
+    }
+    
+    public void ekranOrtala(int eGenislik , int eYukseklik){
+        
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        
+        int posX = (dim.width-eGenislik)/2;
+        int posY = (dim.height-eYukseklik)/2;
+        
+        setBounds(posX, posY, eGenislik, eYukseklik);
+    }
+}
